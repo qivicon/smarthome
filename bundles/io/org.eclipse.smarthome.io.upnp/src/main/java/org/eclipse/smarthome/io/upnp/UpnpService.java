43666e372f233d7d0f67c07246e1f9177de5f787
Andre Fuechsel <a.fuechsel@telekom.de>
2014-07-30 14:18:49 +0200

    initial skeleton
    Signed-off-by: Andre Fuechsel <a.fuechsel@telekom.de>

---
+++ bundles/io/org.eclipse.smarthome.io.upnp/src/main/java/org/eclipse/smarthome/io/upnp/UpnpService.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.upnp;
+
+
+/**
+ * 
+ * 
+ * @author Andre Fuechsel - Initial contribution and API
+ */
+public interface UpnpService {
+	
+
+	
+}
