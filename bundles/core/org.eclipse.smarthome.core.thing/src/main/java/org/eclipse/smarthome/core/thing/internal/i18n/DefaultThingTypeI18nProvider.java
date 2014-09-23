1a19c93fdf0b3d5b7e21666f55c4da5e086d579a
Dennis Nobel <d.nobel@external.telekom.de>
2014-09-23 19:59:20 +0200

    Added channel type localization.
    Signed-off-by: Dennis Nobel <d.nobel@external.telekom.de>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/i18n/DefaultThingTypeI18nProvider.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/i18n/DefaultThingTypeI18nProvider.java
@@ -5,30 +5,42 @@ import java.util.Locale;
 import org.eclipse.smarthome.core.i18n.I18nProvider;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.i18n.ThingTypeI18nProvider;
-
+import org.eclipse.smarthome.core.thing.type.ChannelTypeUID;
 
 public class DefaultThingTypeI18nProvider implements ThingTypeI18nProvider {
 
-    private I18nProvider i18nProvider;
+	private I18nProvider i18nProvider;
 
-    @Override
-    public String getLabel(ThingTypeUID thingTypeUID, String defaultLabel, Locale locale) {
-        return i18nProvider.getText(thingTypeUID.getBindingId(), "thing-type." + thingTypeUID.getId() + ".label",
-                defaultLabel, locale);
-    }
+	@Override
+	public String getLabel(ThingTypeUID thingTypeUID, String defaultLabel, Locale locale) {
+		return i18nProvider.getText(thingTypeUID.getBindingId(), "thing-type." + thingTypeUID.getId() + ".label",
+				defaultLabel, locale);
+	}
 
-    @Override
-    public String getDescription(ThingTypeUID thingTypeUID, String defaultDescription, Locale locale) {
-        return i18nProvider.getText(thingTypeUID.getBindingId(), "thing-type." + thingTypeUID.getId() + ".description",
-                defaultDescription, locale);
-    }
+	@Override
+	public String getDescription(ThingTypeUID thingTypeUID, String defaultDescription, Locale locale) {
+		return i18nProvider.getText(thingTypeUID.getBindingId(), "thing-type." + thingTypeUID.getId() + ".description",
+				defaultDescription, locale);
+	}
 
-    protected void setI18nProvider(I18nProvider i18nProvider) {
-        this.i18nProvider = i18nProvider;
-    }
+	@Override
+	public String getChannelLabel(ChannelTypeUID channelTypeUID, String defaultLabel, Locale locale) {
+		return i18nProvider.getText(channelTypeUID.getBindingId(), "channel-type." + channelTypeUID.getId() + ".label",
+				defaultLabel, locale);
+	}
 
-    protected void unsetI18nProvider(I18nProvider i18nProvider) {
-        this.i18nProvider = null;
-    }
+	@Override
+	public String getChannelDescription(ChannelTypeUID channelTypeUID, String defaultDescription, Locale locale) {
+		return i18nProvider.getText(channelTypeUID.getBindingId(), "channel-type." + channelTypeUID.getId()
+				+ ".description", defaultDescription, locale);
+	}
+
+	protected void setI18nProvider(I18nProvider i18nProvider) {
+		this.i18nProvider = i18nProvider;
+	}
+
+	protected void unsetI18nProvider(I18nProvider i18nProvider) {
+		this.i18nProvider = null;
+	}
 
 }
