a237df78cbdef89cdcdc3caa8e9ae2e6ce64181a
Dennis Nobel <d.nobel@external.telekom.de>
2014-10-06 10:53:17 +0200

    Refactored context specific providers to util classes.

---
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/i18n/BindingI18nUtil.java
@@ -0,0 +1,41 @@
+package org.eclipse.smarthome.core.i18n;
+
+import java.util.Locale;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * The {@link BindingI18nUtil} uses the {@link I18nProvider} to resolve the
+ * localized texts. It automatically infers the key if the default text is not a
+ * constant.
+ * 
+ * @author Dennis Nobel - Initial contribution
+ */
+public class BindingI18nUtil {
+
+    private I18nProvider i18nProvider;
+    
+    public BindingI18nUtil(I18nProvider i18nProvider) {
+        this.i18nProvider = i18nProvider;
+    }
+
+    public String getDescription(Bundle bundle, String bindingId, String defaultDescription, Locale locale) {
+
+        String key = I18nUtil.isConstant(defaultDescription) ? I18nUtil.stripConstant(defaultDescription) : inferKey(
+                bindingId, "description");
+
+        return i18nProvider.getText(bundle, key, defaultDescription, locale);
+    }
+
+    public String getLabel(Bundle bundle, String bindingId, String defaultLabel, Locale locale) {
+        String key = I18nUtil.isConstant(defaultLabel) ? I18nUtil.stripConstant(defaultLabel) : inferKey(bindingId,
+                "label");
+
+        return i18nProvider.getText(bundle, key, defaultLabel, locale);
+    }
+
+    private String inferKey(String bindingId, String lastSegment) {
+        return "binding." + bindingId + "." + lastSegment;
+    }
+
+}
--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/i18n/I18nUtil.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/i18n/I18nUtil.java
@@ -1,17 +1,17 @@
 package org.eclipse.smarthome.core.i18n;
 
 
-public class ContextSpecificI18nProvider {
+public class I18nUtil {
 
 	/** The 'text' pattern (prefix) which marks constants. */
     private static final String CONSTANT_PATTERN = "@text/";
 
 
-	protected boolean isConstant(String key) {
+	public static boolean isConstant(String key) {
 		return key != null && key.startsWith(CONSTANT_PATTERN);
 	}
 
-	protected String stripConstant(String key) {
+	public static String stripConstant(String key) {
 		return key.replace(CONSTANT_PATTERN, "");
 	}
 
