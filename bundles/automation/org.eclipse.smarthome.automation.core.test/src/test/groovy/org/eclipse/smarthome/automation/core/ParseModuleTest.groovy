15f3a7c63e7de3d37690553bd950e6347f62f924
Benedikt Niehues <benedikt.niehuesa@itemis.de>
2014-10-17 10:55:16 +0200

    tests for parsing automation json strings

--- bundles/automation/org.eclipse.smarthome.automation.core.test/src/test/groovy/org/eclipse/smarthome/automation/core/ParseModuleTest.groovy
+++ bundles/automation/org.eclipse.smarthome.automation.core.test/src/test/groovy/org/eclipse/smarthome/automation/core/ParseModuleTest.groovy
@@ -7,10 +7,15 @@ import org.eclipse.smarthome.automation.core.jsonmodel.Module
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test
 
+/**
+ * Test class for parsing json strings containing module definitions.
+ * @author niehues
+ *
+ */
 class ParseModuleTest {
 
 	@Test
-	public void test() {
+	public void testMailModule() {
 		String json = """{
             "name": "SendMail",
             "moduleType": "action",
@@ -34,7 +39,7 @@ class ParseModuleTest {
                 {
                     "name": "body",
                     "type": "text",
-                    "label": "Recipient",
+                    "label": "Body",
                     "required": false
                 }
             ]
@@ -43,6 +48,94 @@ class ParseModuleTest {
 		def Module module = objectMapper.readValue(json, Module);
 		assertThat module, is(notNullValue())
 		assertEquals(module.name, "SendMail")
+		assertEquals(module.moduleType,"action")
+		assertEquals(module.description,"Allows to send a Mail")
+		assertEquals(module.label,"Send an email")
+		assertEquals(module.shortcut, "@")
+		def inputParameters = module.inputParameters
+		assertNotNull(inputParameters)
+		assertNull(module.outputParameters)
+		assertFalse inputParameters.empty
+		for (inputParam in inputParameters) {
+			switch (inputParam.name){
+				case "recipient":
+					assertEquals(inputParam.context, "email")
+					assertEquals(inputParam.label,"Recipient")
+					assertEquals(inputParam.required, true)
+					assertEquals(inputParam.type, "text")
+					break
+				case "subject":
+					assertEquals(inputParam.type, "text")
+					assertEquals(inputParam.label, "Recipient")
+					assertEquals(inputParam.required, true)
+					break
+				case "body":
+					assertEquals(inputParam.type, "text")
+					assertEquals(inputParam.label, "Body")
+					assertEquals(inputParam.required, false)
+					break
+				default: fail("inputParameter not expected: " + inputParam.name)
+			}
+		}
 	}
 
+	@Test
+	void testTimerModule() {
+		def json = """{
+			"name": "Timer",
+			"moduleType": "trigger",
+			"label": "Timer",
+			"shortcut": "cron",
+			"inputParameters": [
+				{
+					"name": "timer",
+					"type": "text",
+					"label": "Time",
+					"context": "cron",
+					"required": true
+				}
+			],
+			"outputParameters": [
+				{
+					"name": "time",
+					"type": "datetime",
+					"label": "Time of the trigger"
+				}
+			]
+		}"""
+		def objectMapper = new ObjectMapper();
+		def Module module = objectMapper.readValue(json, Module);
+		assertThat module, is(notNullValue())
+		assertEquals(module.name, "Timer")
+		assertEquals(module.moduleType, "trigger")
+		assertNull(module.description)
+		assertEquals(module.label,"Timer")
+		assertEquals(module.shortcut, "cron")
+		def inputParameters = module.inputParameters
+		assertNotNull(inputParameters)
+		assertFalse inputParameters.empty
+		for (inputParam in module.inputParameters) {
+			switch(inputParam.name){
+				case "timer":
+					assertEquals(inputParam.type, "text")
+					assertEquals(inputParam.label, "Time")
+					assertEquals(inputParam.context, "cron")
+					assertEquals(inputParam.required, true)
+					break
+				default:
+					fail("inputParameter " + inputParam.name + " is not expected")
+			}
+		}
+		for (outParam in module.outputParameters) {
+			switch(outParam.name){
+				case "time":
+					assertEquals(outParam.label,"Time of the trigger")
+					assertEquals(outParam.type, "datetime")
+
+					break
+				default:
+					fail("outputParameter " + outputParam.name + " is not expected")
+			}
+		}
+	}
 }
---
+++ bundles/automation/org.eclipse.smarthome.automation.core.test/src/test/groovy/org/eclipse/smarthome/automation/core/ParseRuleTest.groovy
@@ -0,0 +1,83 @@
+package org.eclipse.smarthome.automation.core;
+
+import static org.junit.Assert.*;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.smarthome.automation.core.jsonmodel.ModuleRef;
+import org.eclipse.smarthome.automation.core.jsonmodel.Rule;
+import org.junit.Test;
+
+class ParseRuleTest {
+
+	@Test
+	public void test() {
+		def json = """{
+						  "name" : "Heat up in the Morning",
+						  "enabled": true,
+						  "on": [
+						    {
+						      "type": "Timer",
+						      "parameters": {
+						          "time": "30 6 * * *"
+						      }
+						    }
+						  ],
+						  "if": [
+						    {
+						      "type": "Compare",
+						      "parameters": {
+						        "operator": "<",
+						        "left:": "TemperatureItem",
+						        "right": "18"
+						      }
+						    }
+						  ],
+						  "then": [
+						    {
+						      "type": "SendCommand",
+						      "parameters": {
+						        "itemName": "TemperatureActuator",
+						        "command": "23"
+						      }
+						    }
+						  ]
+						}
+		 """
+
+		def mapper = new ObjectMapper()
+		def Rule rule = mapper.readValue(json, Rule)
+		assertNotNull rule
+		assertEquals(rule.name, "Heat up in the Morning")
+		assertEquals(rule.enabled, true)
+		assertNotNull(rule._if)
+		assertNotNull(rule._then)
+		assertNotNull(rule.on)
+		assertFalse(rule._if.empty)
+		assertFalse(rule._then.empty)
+		assertFalse(rule.on.empty)
+		assertEquals(rule._if.size(), 1)
+		assertEquals(rule._then.size(),1)
+		assertEquals(rule.on.size(), 1)
+		
+		def ModuleRef timer = rule.on.get(0)
+		assertEquals(timer.type,"Timer")
+		assertNotNull(timer.parameters)
+		assertFalse(timer.parameters.empty)
+		assertEquals(timer.parameters.get("time"),"30 6 * * *")
+		
+		def ModuleRef compare = rule._if.get(0)
+		assertEquals(compare.type, "Compare")
+		assertNotNull(compare.parameters)
+		assertEquals(compare.parameters.size(),3)
+		assertEquals(compare.parameters.get("operator"),"<")
+		assertEquals(compare.parameters.get("left"),"TemperatureItem")
+		assertEquals(compare.parameters.get("right"),"18")
+
+		def ModuleRef sendCommand = rule._then.get(0)
+		assertEquals(sendCommand.type,"SendCommand")
+		assertNotNull(sendCommand.parameters)
+		assertEquals(sendCommand.parameters.size(),2)
+		assertEquals(sendCommand.parameters.get("itemName"),"TemperatureActuator")
+		assertEquals(sendCommand.parameters.get("command"),"23")
+	}
+}
